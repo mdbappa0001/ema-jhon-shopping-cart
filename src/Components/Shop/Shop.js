fc57514d09d6231c19a9e8d0dd9cea973530c8b6
mdbappa0001 <abutanif366@gmail.com>
2022-03-23 03:36:54 +0600

    connect with local storage

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb} from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -15,10 +16,11 @@ const Shop = () => {
 
 
     const handleAddToCart = (product) => {
-        console.log(product);
+        // console.log(product);
         // cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
 
